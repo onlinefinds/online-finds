2ed0426181a346ea4fcfe5b35c9c4258c0032a8a
onlinefinds <jayden.burger@gmx.ch>
2026-07-02 22:51:44 +0200

    Update products.js

--- products.js
+++ products.js
@@ -31,3 +31,52 @@ const PRODUCTS = [
     featured: true,
     affiliateUrl: "https://amzn.to/4eUS5iS"
   },
+ {
+    id: 3,
+    title: "GLORIOUS Gaming Mauspad ",
+    category: "tech",
+    image: "https://www.amazon.de/Glorious-Gaming-Mauspad-Spritzwasserfestes-Gaming-Sensoren-Stealth/dp/B0DJ9TTWCR/ref=sr_1_2?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=SJA15FX1QGFM&dib=eyJ2IjoiMSJ9.LmRTJcDlW8UxhQ4EDxyKVWJBm_AGBSDLrrvYFZL_8MTcK7AE3MSRsVkoW5iL4uqvADH5GvCIrkmmykSQfGDeaOm6aknjy-n4ARturzxihoqg-alROEWwu-k8fi1UWvqyj1Kxxf7obyQKRBgjVbUQrYB6qAE-IRr80HUZoMJBHpD0LspWfI8aJ_ZuBQZ9AsK0Vf-pzXykC_6FK6Mb9FwiYLKBnOowHq6avtC79mAZ3sA.gFsfvZmK_iL9gZ6uFQqEZvJp7fdBX5ou8RMAtZXGUa4&dib_tag=se&keywords=glorious%2Bmauspad&qid=1783017692&sprefix=glorius%2Bmauspad%2Cspecialty-aps%2C128&sr=8-2&th=1",
+    price: 22.70,
+    oldPrice: 22.70,
+    rating: 4.6,
+    reviews: 137,
+    featured: true,
+    affiliateUrl: "https://amzn.to/4vagmry"
+  },
+   {
+    id: 4,
+    title: "3 in 1 Ladestation",
+    category: "tech",
+    image: "https://m.media-amazon.com/images/I/51vzpftxZtL._AC_SL1024_.jpg",
+    price: 34.51,
+    oldPrice: 41.78,
+    rating: 4.6,
+    reviews: 76,
+    featured: true,
+    affiliateUrl: "https://amzn.to/4vcfpip"
+  },
+   {
+    id: 5,
+    title: "Handventilator Tragbarer Turbo-Ventilator ",
+    category: "tech",
+    image: "https://m.media-amazon.com/images/I/71v27TDiGAL._AC_SL1500_.jpg",
+    price: 26.33,
+    oldPrice: 26.33,
+    rating: 4.7,
+    reviews: 1578,
+    featured: true,
+    affiliateUrl: "https://amzn.to/4eIBgcb"
+  },
+   {
+    id: 6,
+    title: "Pomodoro Timer Cube",
+    category: "tech",
+    image: "https://m.media-amazon.com/images/I/61MYYMQvzDL._AC_SL1500_.jpg",
+    price: 18.16,
+    oldPrice: 18.16,
+    rating: 4.6,
+    reviews: 136,
+    featured: true,
+    affiliateUrl: "https://amzn.to/4xYnDNC"
+  },
+  
